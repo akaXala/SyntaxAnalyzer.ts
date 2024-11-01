dcbe1a81883b211523313a553453a9d3919b84a5
akaXala <144200243+akaXala@users.noreply.github.com>
2024-11-01 16:08:22 -0600

    Cambios menores

--- ts/prueba.ts
+++ ts/prueba.ts
@@ -1,10 +1,18 @@
 // Prueba
-
-import { execSync } from "child_process";
+import prueba2 from "@/ts/tools/prueba2";
 
 class Prueba {
+    hola: Set<string> = new Set();
+    pr = new prueba2();
+
     constructor() {
-        console.log('Prueba');
+        this.hola.add("Hola ");
+    }
+
+    imprimirHolaMundo(): void {
+        const hola = Array.from(this.hola).join(''); // Convierte el Set a string
+        const mundo = Array.from(this.pr.getMundo()).join(''); // Obtiene el Set de prueba2 y lo convierte a string
+        console.log(hola + mundo); // Imprime "Hola mundo"
     }
 }
 
